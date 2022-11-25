ef6a4a15f0a237788fc93b79334dd325fd7ca5da
senees <23065216+senees@users.noreply.github.com>
2022-11-25 13:46:02 +0100

    Refactoring.

--- src/decimal128.rs
+++ src/decimal128.rs
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2021 Dariusz Depta Engos Software
+ * Copyright (c) 2021 senees
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
